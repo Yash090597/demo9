b40b93081e6e964ded5eba66625bb49d1104933d
Yash090597 <yashlodalliwar@gmail.com>
2021-07-08 23:06:44 +0530

    login commited

---
+++ qwerty/src/main/java/qwerty/loginpage.java
@@ -0,0 +1,10 @@
+package qwerty;
+
+public class loginpage {
+
+	public static void main(String[] args) {
+		System.out.println("welcome in login page");
+
+	}
+
+}
---
+++ qwerty/src/main/java/qwerty/validationqw.java
@@ -0,0 +1,10 @@
+package qwerty;
+
+public class validationqw {
+
+	public static void main(String[] args) {
+		System.out.println("welcome to validate");
+
+	}
+
+}
